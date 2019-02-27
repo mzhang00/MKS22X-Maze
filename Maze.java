89c9b54a3969d47bdcee53762e3021dddf1465a2
Michael Zhang <mzhang00@stuy.edu>
2019-02-27 09:57:14 -0500

    Minor updates to maze

--- Maze.java
+++ Maze.java
@@ -3,25 +3,7 @@ import java.io.*;
 public class Maze{
 
     private char[][]maze;
-    private boolean animate;//false by default
-
-    /*Constructor loads a maze text file, and sets animate to false by default.
-
-      1. The file contains a rectangular ascii maze, made with the following 4 characters:
-      '#' - Walls - locations that cannot be moved onto
-      ' ' - Empty Space - locations that can be moved onto
-      'E' - the location of the goal (exactly 1 per file)
-
-      'S' - the location of the start(exactly 1 per file)
-
-      2. The maze has a border of '#' around the edges. So you don't have to check for out of bounds!
-
-
-      3. When the file is not found OR the file is invalid (not exactly 1 E and 1 S) then:
-
-         throw a FileNotFoundException or IllegalStateException
-
-    */
+    private boolean animate;
 
     public Maze(String filename) throws FileNotFoundException{
       animate = false;
@@ -40,6 +22,13 @@ public class Maze{
         for (int c = 0; c < cols; c++){
           //maze[r][c] =
           System.out.println(maze[r][c]);
+        }
+      }
+      int SCount = 0;
+      int ECount = 0;
+      for (int r = 0; r < rows; r++){
+        for (int c = 0; c < cols; c++){
+          //if ()
         }
       }
     }
@@ -86,7 +75,7 @@ public class Maze{
 
             //and start solving at the location of the s.
 
-            //return solve(???,???);
+            return solve(0,0);
 
     }
 
