2c52c91cc1676a3ceb472a97a24e03c2928323e8
Michael Zhang <mzhang00@stuy.edu>
2019-02-27 09:52:22 -0500

    Create Maze file, add text mazes

--- ReadFile.java
+++ ReadFile.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class ReadFile {
   public static void main(String args[]){
     try{
@@ -8,12 +10,19 @@ public class ReadFile {
       Scanner inf = new Scanner(text);
       int rows = 0;
       int cols = 0;
+      ArrayList<String> columns = new ArrayList<>();
       while(inf.hasNextLine()){
         String line = inf.nextLine();
         cols = line.length();
         rows++;
       }
       String[][] maze = new String[rows][cols];
+      for (int r = 0; r < rows; r++){
+        for (int c = 0; c < cols; c++){
+          //maze[r][c] =
+          System.out.println(maze[r][c]);
+        }
+      }
     }catch(FileNotFoundException e){
       System.out.println("File does not exist. Try again with a valid file.");
     }
