d5dae1472224788530a9d5edf094b165dc83d74b
Michael Zhang <mzhang00@stuy.edu>
2019-03-03 03:46:16 -0500

    Test to make sure constructor throws proper exceptions, update spacing

--- Driver.java
+++ Driver.java
@@ -1,25 +1,26 @@
 import java.io.FileNotFoundException;
 
 public class Driver{
-    public static void main(String[]args){
-      String filename = "data2.dat";
-      //String filename = "Maze1.txt";
-      try{
-        Maze f;
-        f = new Maze(filename);
-        /*
-        f.setAnimate(true);
-        f.solve();
-        */
-        
-        f.setAnimate(true);
-        System.out.println(f.solve());
-        
-        System.out.println("-------------------------------");
-        System.out.println(f);
-                
-      }catch(FileNotFoundException e){
-        System.out.println("Invalid filename: "+filename);
-      }
+  public static void main(String[] args){
+    //String filename = "dataz.dat";
+    String filename = "data3.dat";
+    //String filename = "Maze1.txt";
+
+    try{
+      Maze f;
+      f = new Maze(filename);
+
+      /*f.setAnimate(true);
+      f.solve();*/
+      
+      f.setAnimate(true);
+      System.out.println(f.solve());
+      
+      System.out.println("-------------------------------");
+      System.out.println(f);
+              
+    }catch(FileNotFoundException e){
+      System.out.println("Invalid filename: " + filename);
     }
+  }
 }
--- Maze.java
+++ Maze.java
@@ -2,186 +2,181 @@ import java.util.*;
 import java.io.*;
 
 public class Maze{
+  private char[][] maze;
+  private boolean animate;
+  private int[] xmoves = {0, 1, 0, -1};
+  private int[] ymoves = {-1, 0, 1, 0};
 
-    private char[][] maze;
-    private boolean animate;
-    private int[] xmoves = {0, 1, 0, -1};
-    private int[] ymoves = {-1, 0, 1, 0};
-
-    public Maze(String filename) throws FileNotFoundException{
-      animate = false;
-      File text = new File(filename);
-      Scanner inf = new Scanner(text);
-      int rows = 0;
-      int cols = 0;
-      ArrayList<String> columns = new ArrayList<>();
-      while(inf.hasNextLine()){
-        String line = inf.nextLine();
-        cols = line.length();
-        columns.add(line);
-        rows++;
-      }
-      maze = new char[rows][cols];
-      for (int r = 0; r < rows; r++){
-        for (int c = 0; c < cols; c++){
-          maze[r][c] = columns.get(r).charAt(c);
-        }
-      }
-      int Scount = 0;
-      int Ecount = 0;
-      for (int a = 0; a < maze.length; a++){
-        for (int b = 0; b < maze[0].length; b++){
-          if (maze[a][b] == 'S'){
-            Scount++;
-          }
-          if (maze[a][b] == 'E'){
-            Ecount++;
-          }
-        }
-      }
-      if (Scount != 1 || Ecount != 1){
-        throw new IllegalStateException();
+  public Maze(String filename) throws FileNotFoundException{
+    animate = false;
+    File text = new File(filename);
+    Scanner inf = new Scanner(text);
+    int rows = 0;
+    int cols = 0;
+    ArrayList<String> columns = new ArrayList<>();
+    while(inf.hasNextLine()){
+      String line = inf.nextLine();
+      cols = line.length();
+      columns.add(line);
+      rows++;
+    }
+    maze = new char[rows][cols];
+    for (int r = 0; r < rows; r++){
+      for (int c = 0; c < cols; c++){
+        maze[r][c] = columns.get(r).charAt(c);
       }
     }
-
-    private void wait(int millis){
-      try {
-        Thread.sleep(millis);
-      }
-      catch (InterruptedException e) {
-      }
-     }
-
-
-    public void setAnimate(boolean b){
-      animate = b;
-    }
-
-
-    public void clearTerminal(){
-      System.out.println("\033[2J\033[1;1H");
-    }
-
-
-
-    /*Wrapper Solve Function returns the helper function
-
-      Note the helper function has the same name, but different parameters.
-      Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists.
-
-    */
-    public int solve(){
-      int Sx = 0;
-      int Sy = 0;
-      for (int i = 0; i < maze.length; i++){
-        for (int c = 0; c < maze[0].length; c++){
-          if (maze[i][c] == 'S'){
-            Sx = i;
-            Sy = c;
-            c = maze[0].length;
-            i = maze.length;
-          }
+    int Scount = 0;
+    int Ecount = 0;
+    for (int a = 0; a < maze.length; a++){
+      for (int b = 0; b < maze[0].length; b++){
+        if (maze[a][b] == 'S'){
+          Scount++;
+        }
+        if (maze[a][b] == 'E'){
+          Ecount++;
         }
       }
-      maze[Sx][Sy] = '@';
-      try{
-        solve(Sx, Sy);
+    }
+    if (Scount != 1 || Ecount != 1){
+      throw new IllegalStateException();
+    }
+  }
+
+  private void wait(int millis){
+    try {
+      Thread.sleep(millis);
+    }catch (InterruptedException e){
+    }
+  }
+
+
+  public void setAnimate(boolean b){
+    animate = b;
+  }
+
+
+  public void clearTerminal(){
+    System.out.println("\033[2J\033[1;1H");
+  }
+
+
+
+  /*Wrapper Solve Function returns the helper function
+
+    Note the helper function has the same name, but different parameters.
+    Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists.
+
+  */
+  public int solve(){
+    int Sx = 0;
+    int Sy = 0;
+    for (int i = 0; i < maze.length; i++){
+      for (int c = 0; c < maze[0].length; c++){
+        if (maze[i][c] == 'S'){
+          Sx = i;
+          Sy = c;
+          c = maze[0].length;
+          i = maze.length;
+        }
       }
-      catch(IndexOutOfBoundsException e){
+    }
+    maze[Sx][Sy] = '@';
+    try{
+      solve(Sx, Sy);
+    }catch(IndexOutOfBoundsException e){
+    }
+    int result = -1;
+    for (int r = 0; r < maze.length; r++){
+      for (int c = 0; c < maze[0].length; c++){
+        if (maze[r][c] == '@'){
+          result++;
+        }
       }
-      int result = -1;
+    }
+    if (result < 0){
+      result--;
       for (int r = 0; r < maze.length; r++){
         for (int c = 0; c < maze[0].length; c++){
-          if (maze[r][c] == '@'){
-            result++;
+          if (maze[r][c] != '#' && maze[r][c] != 'E'){
+            maze[r][c] = ' ';
           }
         }
       }
-      if (result < 0){
-        result--;
-        for (int r = 0; r < maze.length; r++){
-          for (int c = 0; c < maze[0].length; c++){
-            if (maze[r][c] != '#' && maze[r][c] != 'E'){
-              maze[r][c] = ' ';
-            }
-          }
-        }
-        maze[Sx][Sy] = 'S';
-      }
-      return result + 1;
+      maze[Sx][Sy] = 'S';
     }
+    return result + 1;
+  }
 
-    /*
-      Recursive Solve function:
+  /*
+    Recursive Solve function:
 
-      A solved maze has a path marked with '@' from S to E.
+    A solved maze has a path marked with '@' from S to E.
 
-      Returns the number of @ symbols from S to E when the maze is solved,
-      Returns -1 when the maze has no solution.
+    Returns the number of @ symbols from S to E when the maze is solved,
+    Returns -1 when the maze has no solution.
 
 
-      Postcondition:
+    Postcondition:
 
-        The S is replaced with '@' but the 'E' is not.
+      The S is replaced with '@' but the 'E' is not.
 
-        All visited spots that were not part of the solution are changed to '.'
+      All visited spots that were not part of the solution are changed to '.'
 
-        All visited spots that are part of the solution are changed to '@'
-    */
-    private int solve(int row, int col){
-      /*if(animate){
-        clearTerminal();
-        char save = maze[row][col];
-        maze[row][col] = '\u2588';
-        System.out.println(this);
-        maze[row][col] = save;
-        wait(200);
+      All visited spots that are part of the solution are changed to '@'
+  */
+
+  private int solve(int row, int col){
+    /*if(animate){
+      clearTerminal();
+      char save = maze[row][col];
+      maze[row][col] = '\u2588';
+      System.out.println(this);
+      maze[row][col] = save;
+      wait(100);
     }*/
-      if(animate){
-        clearTerminal();
-        System.out.println(this);
-        wait(10);
-      }
-      if (maze[row][col] == 'E'){
-        return 0;
-      }
-      for (int i = 0; i <= 3; i++){
-        if (maze[row + xmoves[i]][col + ymoves[i]] == 'E'){
-          throw new IndexOutOfBoundsException();
-        }
-        if (goTo(row + xmoves[i], col + ymoves[i])){
-          solve(row + xmoves[i], col + ymoves[i]);
-        }
-        if (i == 3){
-          maze[row][col] = '.';
-        }
-      }
-      return 0;
+    if(animate){
+      clearTerminal();
+      System.out.println(this);
+      wait(100);
     }
+    for (int i = 0; i <= 3; i++){
+      if (maze[row + xmoves[i]][col + ymoves[i]] == 'E'){
+        throw new IndexOutOfBoundsException();
+      }
+      if (goTo(row + xmoves[i], col + ymoves[i])){
+        solve(row + xmoves[i], col + ymoves[i]);
+      }
+      if (i == 3){
+        maze[row][col] = '.';
+      }
+    }
+    return 0;
+  }
 
-    //row and col are the NEW row and NEW col
-    private boolean goTo(int row, int col){
-      if (maze[row][col] == '#' || maze[row][col] == '@' || maze[row][col] == '.'){
-        return false;
-      }else{
-        maze[row][col] = '@';
-        return true;
-      }
+  //row and col are the NEW row and NEW col  
+  private boolean goTo(int row, int col){
+    if (maze[row][col] == '#' || maze[row][col] == '@' || maze[row][col] == '.'){
+      return false;
+    }else{
+      maze[row][col] = '@';
+      return true;
     }
+  }
 
-    public String toString(){
-      String finalstr = "";
-      for (int i = 0; i < maze.length; i++){
-        for (int c = 0; c < maze[i].length; c++){
-          finalstr = finalstr + maze[i][c];
-            if (c != maze[i].length - 1){
-              finalstr += " ";
-            }
+  public String toString(){
+    String finalstr = "";
+    for (int i = 0; i < maze.length; i++){
+      for (int c = 0; c < maze[i].length; c++){
+        finalstr = finalstr + maze[i][c];
+          if (c != maze[i].length - 1){
+            finalstr += " ";
           }
-          if (i != maze.length - 1){
-            finalstr += "\n";
-          }
-      }
-      return finalstr;
+        }
+        if (i != maze.length - 1){
+          finalstr += "\n";
+        }
     }
+    return finalstr;
+  }
 }
